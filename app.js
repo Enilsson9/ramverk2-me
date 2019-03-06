9383f4658965e9f59339d32e03e9b87cb5fc9852
Edward <edward@nilsson.gt>
2019-03-06 10:04:19 -0600

    Testing chai-http

--- app.js
+++ app.js
@@ -38,14 +38,17 @@ app.use('/register', register);
 
 
 // Start up server
-app.listen(port, () => console.log(`Example API listening on port ${port}!`));
+const server = app.listen(port, () => console.log(`Example API listening on port ${port}!`));
+module.exports = server;
 
 // Add routes for 404 and error handling
 // Catch 404 and forward to error handler
 // Put this last
 app.use((req, res, next) => {
     var err = new Error("Not Found");
-    err.status = 404;
+    err.status = 404;const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+module.exports = server;
     next(err);
 });
 
